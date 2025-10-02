a21f00d5d25a7d2e5742cf2b947aa2814c49594e
DenisLaciak <laciak.denis1@gmail.com>
2025-10-02 17:30:15 +0100

    draw window using pygame

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import pygame
 from constants import *
 
 def main():
-    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+  while True:
+    screen.fill("black")
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
 
 if __name__ == "__main__":
-    main()
+  main()
